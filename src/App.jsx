a84d5ca30723be4c7ee153dadeba1bb588347d89
Jean Le Rocher <jlerocher@gmail.com>
2023-11-03 18:59:24 +0100

    remove frameworks icons; install react-dom & react-router; Adding Home, About, Profile, SignIn & SignOut pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-function App() {
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
 
+export default function App() {
   return (
-    <div></div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-Up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
